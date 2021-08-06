8ff6458923bb3b5ed5d4861da4b1878b72ea440b
jakiur rahman <49196470+jaki729@users.noreply.github.com>
2021-08-06 10:25:18 +0530

    Add files via upload

---
+++ crack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#WELCOME TO GLITCHERS
+
+if [ "$#" != 2 ]; then
+        echo "USAGE: $0 file.gpg wordlist.txt"
+else
+
+while read pass; do
+        gpg --batch --passphrase "$pass" "$1" &> /dev/null
+        if [ "$?" == 0 ]; then
+                echo
+                echo "SUCESS - $pass"
+                echo
+                break
+        else
+                echo "FAILED - $pass"
+        fi
+done < "$2"
+fi
